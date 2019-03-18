2819a933de957a025f3d2ee7b54666fc39397bbd
Felice Forby <forby.1@gmail.com>
2019-03-19 08:32:41 +0900

    add color changing functionality

--- src/Tier2.js
+++ src/Tier2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { getReducedColor } from './randomColorGenerator.js'
+import { getRandomColor, getReducedColor } from './randomColorGenerator.js'
 import Tier3 from './Tier3'
 
 
@@ -7,18 +7,33 @@ export default class Tier2 extends Component {
 
   constructor(props) {
     super(props)
+    
     this.state = {
       childColor: getReducedColor(this.props.color),
     }
   }
+  
+  componentWillReceiveProps(nextProps) {
+    const newColor = nextProps.color;
+    
+    this.setState({
+      childColor: getReducedColor(newColor)
+    })
+  }
+  
+  handleChildClick = (event) => {
+    event.stopPropagation();
+    
+    this.setState({
+      childColor: getRandomColor()
+    });
+  }
 
   render() {
-    // hard coded color values have been added below, though they won't be
-    // present in our solution. What should they be replaced with?
     return (
-      <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
-        <Tier3 color={"#F00"} />
+      <div className="tier2" onClick={this.props.handleChildClick} style={{backgroundColor: this.props.color, color: this.props.color}}>
+        <Tier3 color={this.state.childColor} handleChildClick={this.handleChildClick} />
+        <Tier3 color={this.state.childColor} handleChildClick={this.handleChildClick} />
       </div>
     )
   }
